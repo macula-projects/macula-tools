7adbd2b77e9ca9225ad3ee657d56c74006f5b8fb
Rain Wang <rainsoft@163.com>
2015-11-18 11:06:44 +0800

    init
    第一次提交

---
+++ macula-tools-archetype/src/main/resources/archetype-resources/__rootArtifactId__-repository/src/main/java/repository/DemoApplicationRepository.java
@@ -0,0 +1,39 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Copyright 2010-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ${package}.repository;
+
+import java.util.List;
+
+import ${package}.domain.DemoApplication;
+import org.macula.core.repository.MaculaJpaRepository;
+
+/**
+ * <p> <b>ApplicationRepository</b> 是Application的存储接口. </p>
+ * 
+ * @since 2010-12-29
+ * @author Wilson Luo
+ * @version ${symbol_dollar}Id: DemoApplicationRepository.java 5956 2015-11-17 03:48:27Z wzp ${symbol_dollar}
+ */
+public interface DemoApplicationRepository extends MaculaJpaRepository<DemoApplication, Long> {
+
+	DemoApplication findByAppId(String appId);
+
+	List<DemoApplication> findByAppGroup(String appGroup);
+
+}
---
+++ macula-tools-archetype/src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/demo/service/DemoApplicationService.java
@@ -0,0 +1,65 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Copyright 2010-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ${package}.demo.service;
+
+import java.util.List;
+
+import ${package}.domain.DemoApplication;
+
+
+/**
+ * <p> <b>ApplicationManagerService</b> 是应用管理服务接口. </p>
+ * 
+ * @since 2011-8-17
+ * @author Wilson Luo
+ * @version ${symbol_dollar}Id: DemoApplicationService.java 5956 2015-11-17 03:48:27Z wzp ${symbol_dollar}
+ */
+public interface DemoApplicationService {
+
+	/**
+	 * 获取所有的应用列表
+	 * 
+	 * @return
+	 */
+	List<DemoApplication> getAllApplications();
+
+	/**
+	 * 按应用名称获取指定的Application
+	 * 
+	 * @param name
+	 * @return
+	 */
+	DemoApplication findApplicationByAppId(String appId);
+
+	/**
+	 * 保存应用
+	 * 
+	 * @param application
+	 * @return
+	 */
+	Long saveApplication(DemoApplication application);
+
+	/**
+	 * 删除应用
+	 * 
+	 * @param application
+	 */
+	void deleteApplication(DemoApplication application);
+
+}
